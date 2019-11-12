2f36d6c470d6245898d7644479b29576559c3df6
Taljjaa <jenken131@gmail.com>
2019-11-11 21:42:21 -0800

    added associations to models and migrated schema

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ApplicationRecord
+    has_many :users_recipes
+    has_many :users, through: :users_recipes
+    has_many :recipe_tags 
+    has_many :tags, through: :recipe_tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :users_recipes
+    has_many :recipes, through: :users_recipes
 end
