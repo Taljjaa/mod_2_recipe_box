2e3d9cb0e3df9f1c8b40beba3e58d7b78861eb22
Taljjaa <jenken131@gmail.com>
2019-11-13 09:42:41 -0800

    created username validation and new user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,18 @@ class UsersController < ApplicationController
     end
 
     def create 
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            redirect_to '/'
+        else 
+            flash[:message] = "Username is taken. Please try again."
+            render :new
+        end
+    end
+
+    private 
+    def user_params
+        params.require(:user).permit(:username)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_many :users_recipes
     has_many :recipes, through: :users_recipes
+    validates :username, uniqueness: true
 end
