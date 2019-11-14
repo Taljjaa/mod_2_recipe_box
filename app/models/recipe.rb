c27092d9287ee4ac0d7faca19046c0ecfe02ff00
Taljjaa <jenken131@gmail.com>
2019-11-13 16:22:09 -0800

    added username display and dependent destroy to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
-    has_many :users_recipes
+    has_many :users_recipes, dependent: :destroy
     has_many :users, through: :users_recipes
-    has_many :recipe_tags 
+    has_many :recipe_tags, dependent: :destroy 
     has_many :tags, through: :recipe_tags
     accepts_nested_attributes_for :tags
     validates :name, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :users_recipes
+    has_many :users_recipes, dependent: :destroy
     has_many :recipes, through: :users_recipes
     validates :username, uniqueness: true
 end
