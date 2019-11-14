36264118b69573cff14f7e250ae6bf2e44952ef1
Taljjaa <jenken131@gmail.com>
2019-11-14 10:43:21 -0800

    added 404 logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
     before_action :set_current_user
 
+    def not_found
+        raise ActionController::RoutingError.new('Not Found')
+    rescue
+        render_404
+    end
+
+    def render_404
+        render file: "#{Rails.root}/public/404", status: :not_found
+      end
+
     private 
 
     def set_current_user
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   resources :recipes
   resources :users_recipes
   resources :users
+  get '*a', to: "application#not_found"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
