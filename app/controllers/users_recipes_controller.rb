dc9394613a8be3265239a35964845cb0becedf2d
Taljjaa <jenken131@gmail.com>
2019-11-14 20:50:40 -0800

    got search working for recipe names

--- app/controllers/users_recipes_controller.rb
+++ app/controllers/users_recipes_controller.rb
@@ -8,8 +8,20 @@ class UsersRecipesController < ApplicationController
 
     def my_recipes
         if session[:user_id]
-            @user = User.find(session[:user_id])
-            @my_recipes = @user.recipes
+            if params[:search] != nil
+                @user = User.find(session[:user_id])
+                @my_recipes = @user.recipes.filter do |recipe| 
+                    recipe.name.downcase.include?(params[:search].downcase) 
+                end
+                # @my_recipes += @user.recipes.each do |recipe|
+                #     recipe.tags.filter do |tag|
+                #         tag.name.downcase.include?(params[:search].downcase)
+                #     end
+                # end.uniq
+            else
+                @user = User.find(session[:user_id])
+                @my_recipes = @user.recipes
+            end
         else
             redirect_to login_path
         end
